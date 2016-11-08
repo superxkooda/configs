b88774887e630dfa52baa0d205a06cd9f5415640
Michael Anghelone <superxkooda@gmail.com>
2016-11-08 11:18:20 -0500

    Started to work out a install script for bash on windows and a high contrast theme.

---
+++ windows/bash/init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+apt-get update;
+apt-get install vim lxterminal build-essential mingw-w64 python-pip 
+#additions to bashrc
+echo "
+#bash on windows does not respect values set in /etc/login.defs
+umask 002
+" >>~/.bashrc;
+
+source ~/.bashrc;
+
+#install apt-vim
+curl -sL https://raw.githubusercontent.com/egalpin/apt-vim/master/install.sh | sh;
+
+apt-vim install -y https://github.com/scrooloose/nerdtree.git
+
+echo '
+autocmd vimenter * NERDTree
+autocmd vimenter * wincmd p
+autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTree") && b:NERDTree.isTabTree()) | q | endif
+set number
+set fileformats=unix
+set mouse=n
+set ttymouse=xterm2
+' >> ~/.vimrc
